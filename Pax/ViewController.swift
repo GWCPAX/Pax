645de76e552daa82410cb075f33617fa3a1a4a8b
user <user@MacBook-Pro.local>
2018-06-27 15:16:15 -0700

    Initial

--- Pax/ViewController.swift
+++ Pax/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func button1(_ sender: Any) {
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+    }
+    
+    @IBAction func button4(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
